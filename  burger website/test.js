2ac6d30373f7db4837db465714dfc4f8524d6c6d
Saptam <saptamroy40@gmail.com>
2024-01-24 14:46:09 +0530

    burgerWebsite

---
+++  burger website/test.js
@@ -0,0 +1,22 @@
+console.log('fffff');
+
+const test = false;
+
+if (2 > 1) {
+	console.log('test is true');
+} else {
+	console.log('test is false');
+}
+let fruits = ['apple', 'banana', 'orange'];
+// for (let i = 0; i < fruits.length; i++) {
+// 	console.log(fruits);
+// }
+// for (let i = 0; i < fruits.length; i++) {
+// 	console.log(fruits[1]);
+// }
+// for (let i = 1; i < fruits.length; i++) {
+// 	console.log(fruits);
+// }
+for (let i = 0; i < fruits.length; i++) {
+	console.log(fruits[2]);
+}
